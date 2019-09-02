55b1b0e5b823a4d06ca31fbba43f99a0ea950aee
To Hoang Duy <duy.to@firstidea.vn>
2019-09-02 22:22:56 +0700

    revert do choi ngu

---
+++ docs/precache-manifest.526ff0b55633be79f802e30ab9c48c6c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "467c4e407f4d66596e6431a27dd76dfe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "526331e481f92e59745b",
+    "url": "/static/css/3.cee70754.chunk.css"
+  },
+  {
+    "revision": "3562e6ffc8a80c3c3a1a",
+    "url": "/static/js/2.ad88f469.chunk.js"
+  },
+  {
+    "revision": "526331e481f92e59745b",
+    "url": "/static/js/3.daca757c.chunk.js"
+  },
+  {
+    "revision": "646184500256c2c563be",
+    "url": "/static/js/4.43d8d534.chunk.js"
+  },
+  {
+    "revision": "96f43f64e7010fcd7099",
+    "url": "/static/js/5.df6d6990.chunk.js"
+  },
+  {
+    "revision": "9934d26c5e756b1c58db",
+    "url": "/static/js/main.0cec1ee0.chunk.js"
+  },
+  {
+    "revision": "d091ae19e4436888ba78",
+    "url": "/static/js/runtime~main.4237420b.js"
+  }
+]);
---
+++ src/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function About() {
+	return (
+		<section>
+			<Link to="/">Back</Link>
+			<h1>Author is To Hoang Duy</h1>
+		</section>
+	);
+}
+
+export default About;
